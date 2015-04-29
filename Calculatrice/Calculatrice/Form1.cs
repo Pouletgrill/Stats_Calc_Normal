b684f87d767b1491c0d2705de69a0104c76e259a
PouletGrill <xavierb.2006@hotmail.com>
2015-04-29 08:50:08 -0400

    -_-

---
+++ Calculatrice/Calculatrice/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculatrice
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+         CB_Cas.SelectedIndex = 0;
+      }
+
+      private void CB_Cas_SelectedIndexChanged(object sender, EventArgs e)
+      {
+         if (CB_Cas.SelectedIndex == 0)
+         {
+            LB_b.Visible = true;
+            TB_b.Visible = true;
+         }
+         else
+         {
+            LB_b.Visible = false;
+            TB_b.Visible = false;
+         }
+      }
+   }
+}
